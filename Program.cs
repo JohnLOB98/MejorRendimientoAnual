eae851ef882fc4157c217e1fa09c68be8b92eb3f
Admin <Admin@DESKTOP-ODUK8BV>
2024-09-06 20:35:16 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double fondoSemanal     = 1031.84;
+double rendimiento      = 0.000375;
+double mejorRendimiento = 0; 
+int    mejorSemana      = 0;
+
+
+for (int week = 1; week <= 51; week++)
+{
+    int    diasFaltantes = 7 * (52 - week);
+    double cantidadInicial, cantidad;
+
+    cantidadInicial = cantidad = fondoSemanal * week;
+
+    while(--diasFaltantes > 0)
+        cantidad += cantidad * rendimiento;
+
+    double generadoRendimientos = cantidad - cantidadInicial;
+
+    Console.WriteLine($"WEEK {week} --- ${generadoRendimientos.ToString(".00")}");
+    if (generadoRendimientos > mejorRendimiento)
+    {
+        mejorRendimiento = generadoRendimientos;
+        mejorSemana      = week; 
+    }
+
+}
+
+Console.WriteLine($"Para obtener mejor rendimeinto sacar el dinero en la SEMANA {mejorSemana}");
